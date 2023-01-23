1fe35ca4ccc0ad6505a2f18273ccf738932def28
FedericoCopes <federico.copes@gmail.com>
2023-01-23 22:29:00 +0100

    Modifica login e registrazione
    Uso dello username al posto della mail come identificativo nel database di Firebase

--- Navbar/app/src/main/java/com/example/navbar/Login.java
+++ Navbar/app/src/main/java/com/example/navbar/Login.java
@@ -1,5 +1,6 @@
 package com.example.navbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,14 +11,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://mypet-756fb-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText email = findViewById(R.id.email);
+        final EditText username = findViewById(R.id.username);
         final EditText password = findViewById(R.id.password);
         final Button loginBtn = findViewById(R.id.loginBtn);
         final TextView registerNowBtn = findViewById(R.id.registerNowBtn);
@@ -26,14 +35,46 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                final String emailTxt = email.getText().toString();
+                final String usernameTxt = username.getText().toString();
                 final String passwordTxt = password.getText().toString();
 
-                if(emailTxt.isEmpty() || passwordTxt.isEmpty()){
-                    Toast.makeText(Login.this, "Please enter your email or password", Toast.LENGTH_SHORT).show();
+                if(usernameTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(Login.this, "Please enter your username or password", Toast.LENGTH_SHORT).show();
                 }
                 else{
 
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            // check if username is exist in firebase database
+                            if(snapshot.hasChild(usernameTxt)){
+
+                                // username is exist in firebase database
+                                // now get password of user from firebase data and match it with user entered password
+                                final String getPassword = snapshot.child(usernameTxt).child("password").getValue(String.class);
+
+                                if(getPassword.equals(passwordTxt)){
+                                    Toast.makeText(Login.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+
+                                    // open MainActivity on success
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(Login.this, "Wrong Username", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
         });
